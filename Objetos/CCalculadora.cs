aed93535b65f33ce0bdbd758dffab2081150fbf9
KarlosTamayo <srcarlos0252@gmail.com>
2020-05-12 22:09:01 -0500

    primer proyecto

---
+++ Objetos/CCalculadora.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos
+{
+    class CCalculadora
+    {
+        public double a,b,r;
+        public void restar()
+        {
+            r = a - b;
+        }
+
+        public void imprimir()
+
+    
+
+
+
+
+
+        }
+}   }
+         
+
+
